8d7f2f1b895fca272ddbc238e415d573f179f71b
StephanoWurttele <39769681+StephanoWurttele@users.noreply.github.com>
2019-04-05 12:46:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
-
+#include "Vector.h"
 
 int main() {
-
     // Creando vectores
     std::vector<int> vec1;
     UTEC::vector vec2;
@@ -13,11 +12,9 @@ int main() {
     for (int i = 0; i < 100; ++i) {
         vec1.push_back(i);
         vec2.push_back(i);
-
         // Verificando cada dato
         assert(vec1[i] == vec2.get_item(i));
     }
-
     assert(vec1.size() == vec2.size());
     std::cout << "Paso push_back\n";
 
@@ -27,7 +24,7 @@ int main() {
         vec2.pop_back();
     }
 
-    assert(vec1.size() == vec2.size());
+    assert(vec1.size() == vec2.size()); 
 
     // Verificando cada dato
     for (int j = 0; j < vec1.size(); ++j) {
@@ -45,6 +42,7 @@ int main() {
     assert(vec1.size() == vec2.size());
 
     // Verificando cada dato
+
     for (int i = 0; i < vec1.size(); ++i) {
         assert(vec1[i] == vec2.get_item(i));
     }
